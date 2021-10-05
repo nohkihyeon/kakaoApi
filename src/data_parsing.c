03f8ac2121b4b438a8f0ce2acaf04d7149df1097
nohkihyeon <hae8573@koreatech.ac.kr>
2021-10-05 11:09:30 +0900

    예외처리

--- src/data_parsing.c
+++ src/data_parsing.c
@@ -35,7 +35,6 @@ json_t *load_json(const char *text) {
 }
 
 int dataParsing(char* data){
-	int i;
 	json_t *root;
 	char* afterData;
 
@@ -43,6 +42,8 @@ int dataParsing(char* data){
 	json_t *array_value;
 	const char *key;
 	json_t *value;
+	const char *same_nameKey;
+	json_t *same_nameValue;
 
 	afterData = strstr(data, "{");
 	if(afterData == NULL)
@@ -53,53 +54,74 @@ int dataParsing(char* data){
 
 	json_t *jsonData = json_object_get(root, "documents");
 	json_array_foreach(jsonData, index, array_value){ 
-		json_object_foreach(array_value, key, value){
-			if(strstr(key, "address_name"))
-				strcpy(dataSet.documents[i].address_name , json_string_value(value));
-			else if(strcpy(key, "category_group_code"))
-				strstr(dataSet.documents[i].category_group_code , json_string_value(value));
-			else if(strcpy(key, "category_group_name"))
-				strstr(dataSet.documents[i].category_group_name , json_string_value(value));
-			else if(strcpy(key, "category_name"))
-				strstr(dataSet.documents[i].category_name , json_string_value(value));
-			else if(strcpy(key, "distance"))
-				strstr(dataSet.documents[i].distance , json_string_value(value));
-			else if(strcpy(key, "id"))
-				strstr(dataSet.documents[i].id , json_string_value(value));
-			else if(strcpy(key, "phone"))
-				strstr(dataSet.documents[i].phone , json_string_value(value));
-			else if(strcpy(key, "place_name"))
-				strstr(dataSet.documents[i].place_name , json_string_value(value));
-			else if(strcpy(key, "place_url"))
-				strstr(dataSet.documents[i].place_url , json_string_value(value));
-			else if(strcpy(key, "road_address_name"))
-				strstr(dataSet.documents[i].road_address_name  , json_string_value(value));
-			else if(strcpy(key,"x"))
-				strstr(dataSet.documents[i].x , json_string_value(value));
-			else if(strcpy(key,"y"))
-				strstr(dataSet.documents[i].y , json_string_value(value));
-			else
-				error_handling("not found structure");
-			//printf("%s : %s\n",key, json_string_value(value));
+		if(json_typeof(array_value) == JSON_OBJECT){
+			json_object_foreach(array_value, key, value){
+				if(strstr(key, "address_name"))
+					strcpy(dataSet.documents[index].address_name , json_string_value(value));
+				else if(strstr(key, "category_group_code"))
+					strcpy(dataSet.documents[index].category_group_code , json_string_value(value));
+				else if(strstr(key, "category_group_name"))
+					strcpy(dataSet.documents[index].category_group_name , json_string_value(value));
+				else if(strstr(key, "category_name"))
+					strcpy(dataSet.documents[index].category_name , json_string_value(value));
+				else if(strstr(key, "distance"))
+					strcpy(dataSet.documents[index].distance , json_string_value(value));
+				else if(strstr(key, "id"))
+					strcpy(dataSet.documents[index].id , json_string_value(value));
+				else if(strstr(key, "phone"))
+					strcpy(dataSet.documents[index].phone , json_string_value(value));
+				else if(strstr(key, "place_name"))
+					strcpy(dataSet.documents[index].place_name , json_string_value(value));
+				else if(strstr(key, "place_url"))
+					strcpy(dataSet.documents[index].place_url , json_string_value(value));
+				else if(strstr(key, "road_address_name"))
+					strcpy(dataSet.documents[index].road_address_name  , json_string_value(value));
+				else if(strstr(key,"x"))
+					strcpy(dataSet.documents[index].x , json_string_value(value));
+				else if(strstr(key,"y"))
+					strcpy(dataSet.documents[index].y , json_string_value(value));
+				else
+					error_handling("not found structure");
+			}
 		}
-		printf("\naddress_name = %s\ncategory_group_code = %s\ncategory_group_name = %s\ncategory_name = %s\ndistance = %s\nid = %s\nphone = %s\nplace_name = %s\nplace_url = %s\nroad_address_name = %s\nx=%s\ny=%s\n\n\n", dataSet.documents[i].address_name, dataSet.documents[i].category_group_code, dataSet.documents[i].category_group_name, dataSet.documents[i].category_name, dataSet.documents[i].distance, dataSet.documents[i].id,dataSet.documents[i].phone, dataSet.documents[i].place_name, dataSet.documents[i].place_url,dataSet.documents[i].road_address_name,dataSet.documents[i].x, dataSet.documents[i].y);
+		else
+			error_handling("Not Found Object\n");
+
+		printf("\naddress_name = %s\ncategory_group_code = %s\ncategory_group_name = %s\ncategory_name = %s\ndistance = %s\nid = %s\nphone = %s\nplace_name = %s\nplace_url = %s\nroad_address_name = %s\nx=%s\ny=%s\n\n\n", dataSet.documents[index].address_name, dataSet.documents[index].category_group_code, dataSet.documents[index].category_group_name, dataSet.documents[index].category_name, dataSet.documents[index].distance, dataSet.documents[index].id,dataSet.documents[index].phone, dataSet.documents[index].place_name, dataSet.documents[index].place_url,dataSet.documents[index].road_address_name,dataSet.documents[index].x, dataSet.documents[index].y);
 	}
-	json_t *jsonMetaData = json_object_get(root, "meta");
-	json_t *jsonIs_end = json_object_get(jsonMetaData, "is_end");
-	dataSet.meta.is_end = json_boolean_value(jsonIs_end);
-	json_t *jsonPageable_count = json_object_get(jsonMetaData, "pageable_count");
-	dataSet.meta.pageable_count = json_integer_value(jsonPageable_count);
-	json_t *jsonTotal_count = json_object_get(jsonMetaData, "total_count");
-	dataSet.meta.total_count = json_integer_value(jsonTotal_count);
+
+	jsonData = json_object_get(root, "meta");
+	json_object_foreach(jsonData, key, value){
+		if(strstr(key, "is_end"))
+			dataSet.meta.is_end = json_boolean_value(value);
+		else if(strstr(key, "pageable_count"))
+			dataSet.meta.pageable_count = json_integer_value(value);
+		else if(strstr(key, "same_name")){
+			json_t *jsonSameName = json_object_get(jsonData, "same_name");
+			json_object_foreach(jsonSameName, same_nameKey, same_nameValue){
+
+				if(json_typeof(value) == JSON_OBJECT){
+					if(strstr(same_nameKey, "keyword"))
+						strcpy(dataSet.meta.same_name.keyword , json_string_value(same_nameValue));
+					else if(strstr(same_nameKey, "region"))
+						continue;
+					else if(strstr(same_nameKey, "selected_region"))
+						strcpy(dataSet.meta.same_name.selected_region , json_string_value(same_nameValue));
+					else
+						error_handling("Not Found: same_name\n");
+				}
+				else
+					error_handling("Not Found : same_name Object\n");
+			}
+			//dataSet.meta.pageable_count = json_integer_value(value);
+		}
+		else if(strstr(key, "total_count"))
+			dataSet.meta.total_count= json_integer_value(value);
+		else
+			error_handling("Not Found meta\n");
+	}
+
 	printf("Meta\nis_end = %d\npageable_count = %d\ntotal_count = %d\n",dataSet.meta.is_end, dataSet.meta.pageable_count, dataSet.meta.total_count);
-
-	json_t *jsonSame_name = json_object_get(jsonMetaData, "same_name");
-	json_t *jsonKeyword = json_object_get(jsonSame_name, "keyword");
-	strcpy(dataSet.meta.same_name.keyword , json_string_value(jsonKeyword));
-
-	json_t *jsonSelected_region = json_object_get(jsonSame_name, "selected_region");
-	strcpy(dataSet.meta.same_name.selected_region , json_string_value(jsonSelected_region));
-
 	printf("\nsame_name\nkeyword = %s\n\nselected_region = %s\n", dataSet.meta.same_name.keyword, dataSet.meta.same_name.selected_region);
 
 	return 0;
